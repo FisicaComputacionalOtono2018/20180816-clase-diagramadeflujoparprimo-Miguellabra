5b093682be45313a7a72fb8a886008b8f2e7fde8
Miguellabra <42181817+Miguellabra@users.noreply.github.com>
2018-08-16 12:06:59 -0500

    trabajo para sacar 10

---
+++ p.py
@@ -0,0 +1,23 @@
+#Autor: Miguel Labra Lorenzana
+#Fecha: 16/082018
+#Trabajo
+P=input("Escribe un numero")
+S=12
+A=1
+F=1
+while A<(P-1):
+   A=A*+1
+   R=N%A
+   if R==0:
+      P=0
+   P==1
+while P%2==0:
+   P=P+1
+   if P==1:
+      if P<S:
+         S=S-P
+         P=P+2
+      S=S-1
+      if S==0:
+         print(P)
+print (P) 	   
